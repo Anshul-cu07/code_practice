04e52a4840b50eabdc37a87ed4af619537fcf3ff
Anshul Sharma <anshulsharmakgr@gmail.com>
2024-12-13 20:44:15 +0530

    function

--- day1.js
+++ day1.js
@@ -4,3 +4,14 @@ function sum(num1,num2,num3){
     return(num1+num2+num3)
 }
 sum(23,20,12)
+
+//Write a function to check whether number is greater than 5 or not
+function num(num1){
+    if(num1>5){
+        console.log("True")}
+    else{
+        console.log("False")
+    }
+    return(num1)
+}
+num(4)
