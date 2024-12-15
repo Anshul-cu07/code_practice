32549d34f72b10bb4a17b082eedc1bd4178f0347
Anshul Sharma <anshulsharmakgr@gmail.com>
2024-12-15 18:47:42 +0530

    Array in Js

---
+++ day2.js
@@ -0,0 +1,21 @@
+// tell the array length
+arr0 = [1,2,3,4,5,6]
+console.log(arr0.length);
+
+// make reverse of array
+arr1 = [1,2,3,4,5]
+arr1.reverse();
+console.log(arr1);
+
+
+// remove/add one element from last
+arr2 =  [2,4,5,7,8]
+arr2.pop();
+arr2.push(2);
+console.log(arr2);
+
+// remove one element from first
+arr3 = [7,8,9,6,9]
+arr3.shift();
+console.log(arr3);
+
